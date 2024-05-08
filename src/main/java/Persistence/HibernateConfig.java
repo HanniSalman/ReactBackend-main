dd240f04e7e4ed25abac818bfe86ed7b1398c30b
HanniSalman <hannisalman2510@outlook.com>
2024-05-08 10:33:08 +0200

    c

--- src/main/java/Persistence/HibernateConfig.java
+++ src/main/java/Persistence/HibernateConfig.java
@@ -12,32 +12,51 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 import java.util.Properties;
-//hanni
+
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class HibernateConfig {
 
     private static EntityManagerFactory entityManagerFactory;
 
-    private static EntityManagerFactory buildEntityFactoryConfig() {
-
+    private static EntityManagerFactory getEntityManagerFactoryConfigIsDeployed() {
         try {
             Configuration configuration = new Configuration();
 
             Properties props = new Properties();
 
-            props.put("hibernate.connection.url", System.getenv("DB_URL"));
-            props.put("hibernate.connection.username", System.getenv("DB_USER"));
+            props.put("hibernate.connection.url", System.getenv("CONNECTION_STR") + System.getenv("DB_NAME"));
+            props.put("hibernate.connection.username", System.getenv("DB_USERNAME"));
             props.put("hibernate.connection.password", System.getenv("DB_PASSWORD"));
-            props.put("hibernate.show_sql", "true"); // show sql in console
-            props.put("hibernate.format_sql", "true"); // format sql in console
-            props.put("hibernate.use_sql_comments", "true"); // show sql comments in console
+            props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            props.put("hibernate.connection.driver_class", "org.postgresql.Driver");
+            props.put("hibernate.archive.autodetection", "class");
+            props.put("hibernate.current_session_context_class", "thread");
+            props.put("hibernate.hbm2ddl.auto", "update");
 
-            props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect"); // dialect for postgresql
-            props.put("hibernate.connection.driver_class", "org.postgresql.Driver"); // driver class for postgresql
-            props.put("hibernate.archive.autodetection", "class"); // hibernate scans for annotated classes
-            props.put("hibernate.current_session_context_class", "thread"); // hibernate current session context
-            props.put("hibernate.hbm2ddl.auto", "update"); // hibernate creates tables based on entities
+            return getEntityManagerFactory(configuration, props);
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
 
+    private static EntityManagerFactory buildEntityFactoryConfig() {
+        try {
+            Configuration configuration = new Configuration();
+
+            Properties props = new Properties();
+
+            props.put("hibernate.connection.url", "jdbc:postgresql://localhost:5432/hoteldb?currentSchema=public");
+            props.put("hibernate.connection.username", "postgres");
+            props.put("hibernate.connection.password", "postgres");
+            props.put("hibernate.show_sql", "true");
+            props.put("hibernate.format_sql", "true");
+            props.put("hibernate.use_sql_comments", "true");
+            props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            props.put("hibernate.connection.driver_class", "org.postgresql.Driver");
+            props.put("hibernate.archive.autodetection", "class");
+            props.put("hibernate.current_session_context_class", "thread");
+            props.put("hibernate.hbm2ddl.auto", "update");
 
             return getEntityManagerFactory(configuration, props);
         } catch (Throwable ex) {
@@ -78,15 +97,12 @@ public class HibernateConfig {
     }
 
     private static void getAnnotationConfiguration(Configuration configuration) {
-        // add annotated classes
-        // configuration.addAnnotatedClass(<YOUR ENTITY>.class);
         configuration.addAnnotatedClass(Hotel.class);
         configuration.addAnnotatedClass(Room.class);
         configuration.addAnnotatedClass(User.class);
         configuration.addAnnotatedClass(Role.class);
     }
 
-    //Singleton princippet
     private static EntityManagerFactory getEntityManagerFactoryConfigDevelopment() {
         if (entityManagerFactory == null) entityManagerFactory = buildEntityFactoryConfig();
         return entityManagerFactory;
@@ -98,8 +114,9 @@ public class HibernateConfig {
     }
 
     public static EntityManagerFactory getEntityManagerFactoryConfig(boolean isTest) {
-        if (isTest) getEntityManagerFactoryConfigTest();
+        if (isTest) return getEntityManagerFactoryConfigTest();
+        boolean isDeployed = (System.getenv("DEPLOYED") != null);
+        if (isDeployed) return getEntityManagerFactoryConfigIsDeployed();
         return getEntityManagerFactoryConfigDevelopment();
     }
-
 }
